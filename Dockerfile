342dd4bb2c3f127cfd732440595bb45176233908
bramberry <rshulga6@gmail.com>
2019-01-17 11:39:51 +0300

    make it better

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,5 @@ FROM openjdk:8-jdk-alpine
 VOLUME /tmp
 ARG JAR_FILE=target/demo-0.0.1-SNAPSHOT.jar
 COPY ${JAR_FILE} app.jar
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.jar"]
+CMD gunicorn --bind 0.0.0.0:$PORT wsgi
+CMD ["/usr/bin/java","-Djava.security.egd=file:/dev/./urandom", "-jar", "app.jar"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("/demoherokudeploy")
 public class DemoApplication {
 
     public static void main(String[] args) {
