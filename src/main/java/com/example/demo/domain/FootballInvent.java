b0c18e1da7e02f070e3a4f971ba2ca3ec54072c8
Raman <rshulga6@gmail.com>
2019-05-13 15:50:02 +0300

    add getters and setters

--- src/main/java/com/example/demo/domain/FootballInvent.java
+++ src/main/java/com/example/demo/domain/FootballInvent.java
@@ -1,8 +1,10 @@
 package com.example.demo.domain;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class FootballInvent {
     private String url;
     private Double value;
