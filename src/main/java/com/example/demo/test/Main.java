b264d153f96ac317d04ecc0dffe42c3f926b194b
Raman <rshulga6@gmail.com>
2019-09-24 18:00:21 +0300

    final commit

--- src/main/java/com/example/demo/test/Main.java
+++ src/main/java/com/example/demo/test/Main.java
@@ -1,9 +1,12 @@
 package com.example.demo.test;
 
+import lombok.extern.log4j.Log4j2;
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Log4j2
 public class Main {
     public static void main(String[] args) {
 
@@ -11,6 +14,6 @@ public class Main {
                 .map(Long::parseLong)
                 .distinct()
                 .collect(Collectors.toList());
-        System.out.println(carrierPaymentId);
+        log.info(carrierPaymentId);
     }
 }
