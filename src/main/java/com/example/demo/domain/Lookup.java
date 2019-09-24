95b5e90f33b27d9fb12a24f61f96e67d98111e4c
Raman <rshulga6@gmail.com>
2019-09-24 17:57:01 +0300

    test scudsasdas

--- src/main/java/com/example/demo/domain/Lookup.java
+++ src/main/java/com/example/demo/domain/Lookup.java
@@ -3,7 +3,7 @@ package com.example.demo.domain;
 import lombok.Data;
 
 @Data
-class Lookup {
+public class Lookup {
     private Integer id;
     private String title;
 }
---
+++ src/main/java/com/example/demo/test/Main.java
@@ -0,0 +1,16 @@
+package com.example.demo.test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+
+        List<Long> carrierPaymentId = Arrays.stream("1235,4,8,8,8".split(","))
+                .map(Long::parseLong)
+                .distinct()
+                .collect(Collectors.toList());
+        System.out.println(carrierPaymentId);
+    }
+}
